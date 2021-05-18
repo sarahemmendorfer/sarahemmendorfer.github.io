0541b51b175ce3547b50470c38962c1a61b47050
Sarah Emmendörfer <65626976+sarahemmendorfer@users.noreply.github.com>
2021-05-18 08:34:50 -0300

    Add files via upload

---
+++ js/meuPrograma.js
@@ -0,0 +1,228 @@
+$(document).ready(function(){
+    
+    // Selecionar entre CPF e CNPJ
+    $('select').click(function() {
+        var conteudoPessoa = $("#pessoaFisicaJuridica option:selected").text();
+        switch (conteudoPessoa) {
+            case 'Física':
+                cnpjCpf.placeholder = "CPF";
+                $('#cnpjCpf').attr({
+                    'onkeyup': 'FormataCpf(this,event)',
+                    'maxlength': '14',
+                });
+                break;
+            case 'Jurídica':
+                cnpjCpf.placeholder = "CNPJ";
+                $('#cnpjCpf').attr({
+                    'onkeyup': 'FormataCnpj(this,event)',
+                    'maxlength': '18',
+                });
+                break;
+            default:
+                cnpjCpf.placeholder = "";
+                break;
+    }
+
+});
+
+// Altura do Scroll
+
+window.addEventListener("scroll", function (event) {
+    var scroll = this.scrollY;
+    if(scroll >= 400){
+        $("#botaoRetornoTopo").css("visibility", "visible");
+    }else{
+        $("#botaoRetornoTopo").css("visibility", "hidden");
+    }
+});
+
+// Scroll suave para link interno
+$('.meuScroll').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+// Scroll suave para link interno
+$('.meuScroll2').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 900);
+});
+
+
+   
+})
+
+// Formatação do CNPJ
+function FormataCnpj(campo, teclapres)
+{
+    var tecla = teclapres.keyCode;
+    var vr = new String(campo.value);
+    vr = vr.replace(".", "");
+    vr = vr.replace("/", "");
+    vr = vr.replace("-", "");
+    tam = vr.length + 1;
+    if (tecla != 14)
+    {
+        if (tam == 3)
+            campo.value = vr.substr(0, 2) + '.';
+        if (tam == 6)
+            campo.value = vr.substr(0, 2) + '.' + vr.substr(2, 5) + '.';
+        if (tam == 10)
+            campo.value = vr.substr(0, 2) + '.' + vr.substr(2, 3) + '.' + vr.substr(6, 3) + '/';
+        if (tam == 15)
+            campo.value = vr.substr(0, 2) + '.' + vr.substr(2, 3) + '.' + vr.substr(6, 3) + '/' + vr.substr(9, 4) + '-' + vr.substr(13, 2);
+    }
+}
+
+//Formatação do CPF
+
+function FormataCpf(campo, teclapres) {
+    var tecla = teclapres.keyCode;
+    var vr = new String(campo.value);
+    vr = vr.replace(".", "");
+    vr = vr.replace("/", "");
+    vr = vr.replace("-", "");
+    tam = vr.length + 1;
+    if (tecla != 14)
+    {
+        if (tam == 4)
+            campo.value = vr.substr(0, 3) + '.';
+        if (tam == 7)
+            campo.value = vr.substr(0, 3) + '.' + vr.substr(3, 6) + '.';
+        if (tam == 11)
+            campo.value = vr.substr(0, 3) + '.' + vr.substr(3, 3) + '.' + vr.substr(7, 3) + '-' + vr.substr(11, 2);
+    }
+}
+
+
+// Validação de CPF e CNPJ
+
+function validaCpfCnpj(val) {
+    if (val.length == 14) {
+        var cpf = val.trim();
+     
+        cpf = cpf.replace(/\./g, '');
+        cpf = cpf.replace('-', '');
+        cpf = cpf.split('');
+        
+        var v1 = 0;
+        var v2 = 0;
+        var aux = false;
+        
+        for (var i = 1; cpf.length > i; i++) {
+            if (cpf[i - 1] != cpf[i]) {
+                aux = true;   
+            }
+        } 
+        
+        if (aux == false) {
+            return false; 
+        } 
+        
+        for (var i = 0, p = 10; (cpf.length - 2) > i; i++, p--) {
+            v1 += cpf[i] * p; 
+        } 
+        
+        v1 = ((v1 * 10) % 11);
+        
+        if (v1 == 10) {
+            v1 = 0; 
+        }
+        
+        if (v1 != cpf[9]) {
+            return false; 
+        } 
+        
+        for (var i = 0, p = 11; (cpf.length - 1) > i; i++, p--) {
+            v2 += cpf[i] * p; 
+        } 
+        
+        v2 = ((v2 * 10) % 11);
+        
+        if (v2 == 10) {
+            v2 = 0; 
+        }
+        
+        if (v2 != cpf[10]) {
+            return false; 
+        } else {   
+            return true; 
+        }
+    } else if (val.length == 18) {
+        var cnpj = val.trim();
+        
+        cnpj = cnpj.replace(/\./g, '');
+        cnpj = cnpj.replace('-', '');
+        cnpj = cnpj.replace('/', ''); 
+        cnpj = cnpj.split(''); 
+        
+        var v1 = 0;
+        var v2 = 0;
+        var aux = false;
+        
+        for (var i = 1; cnpj.length > i; i++) { 
+            if (cnpj[i - 1] != cnpj[i]) {  
+                aux = true;   
+            } 
+        } 
+        
+        if (aux == false) {  
+            return false; 
+        }
+        
+        for (var i = 0, p1 = 5, p2 = 13; (cnpj.length - 2) > i; i++, p1--, p2--) {
+            if (p1 >= 2) {  
+                v1 += cnpj[i] * p1;  
+            } else {  
+                v1 += cnpj[i] * p2;  
+            } 
+        } 
+        
+        v1 = (v1 % 11);
+        
+        if (v1 < 2) { 
+            v1 = 0; 
+        } else { 
+            v1 = (11 - v1); 
+        } 
+        
+        if (v1 != cnpj[12]) {  
+            return false; 
+        } 
+        
+        for (var i = 0, p1 = 6, p2 = 14; (cnpj.length - 1) > i; i++, p1--, p2--) { 
+            if (p1 >= 2) {  
+                v2 += cnpj[i] * p1;  
+            } else {   
+                v2 += cnpj[i] * p2; 
+            } 
+        }
+        
+        v2 = (v2 % 11); 
+        
+        if (v2 < 2) {  
+            v2 = 0;
+        } else { 
+            v2 = (11 - v2); 
+        } 
+        
+        if (v2 != cnpj[13]) {   
+            return false; 
+        } else {  
+            return true; 
+        }
+    } else {
+        return false;
+    }
+ }
+
